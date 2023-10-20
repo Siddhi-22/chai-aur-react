60420b420b29a884c1c2c963ab96188fa338e2c3
Siddhi-22 <maharanasiddhi@gmail.com>
2023-10-20 10:28:22 +0530

    change

--- 03TailwindProps/src/App.jsx
+++ 03TailwindProps/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-
 import './App.css';
 import Card from './components/Card';
 
@@ -7,10 +6,15 @@ import Card from './components/Card';
 function App() {
   const [count, setCount] = useState(0);
 
+  let myobj = {
+    username: "Witch",
+    age: 100
+  };
+  let arr = [1, 2, 3, 4];
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-lg text-lg mb-5'>TailWind Test</h1>
-      <Card />    
+      <Card title="React-hindi" />    
     </>
     
   )
