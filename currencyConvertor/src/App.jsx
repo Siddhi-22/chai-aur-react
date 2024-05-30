f042a62566a459531194c5775b58f3ae66ba69aa
Siddhi-22 <maharanasiddhi@gmail.com>
2024-05-30 21:25:22 +0530

    newly added

--- currencyConvertor/src/App.jsx
+++ currencyConvertor/src/App.jsx
@@ -3,11 +3,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className='text-3xl bg-orange-500'>Currency Convertor</h1>
+      <h1 className='text-4xl bg-orange-500'>Currency Convertor</h1>
     </>
   )
 }
