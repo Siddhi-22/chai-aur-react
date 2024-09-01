c208488929afb63faeb1feb7cc9b07773a5d9a89
Siddhi-22 <maharanasiddhi@gmail.com>
2024-09-01 17:27:43 +0530

    final configuring

--- 12MegaProject/src/App.jsx
+++ 12MegaProject/src/App.jsx
@@ -1,15 +1,40 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import {Header, Footer} from './components/index';
+import { Outlet } from 'react-router-dom';
 
-import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}));
+      } else{
+        dispatch(logout());
+      }
+    })
+
+    .finally(() => setLoading(false))
+  }, [])
   
-  
-  return (
-    <>
-      <h1>Blog Post App with App write</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:   {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
-export default App
+export default App;
